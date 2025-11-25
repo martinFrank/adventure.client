3cc9b8a4644c28743bd5df83f56f426547aff882
MartinFrank77 <martin.frank@irix.ch>
2025-11-25 16:06:06 +0100

    fixxed missing import

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./styles/global.css";
 import App from "./App";
-// import { AuthProvider } from "./auth/AuthContext";
+import { AuthProvider } from "./auth/AuthContext";
 import { BrowserRouter } from 'react-router-dom';
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>    
-    // <AuthProvider>
-    //   <BrowserRouter basename="/adventure-game">
-    //     <App />
-    //   </BrowserRouter>
-    // </AuthProvider>    
-  // </React.StrictMode>
-
-  <React.StrictMode>
-    <BrowserRouter basename="/adventure-game">
-      <App />
-    </BrowserRouter>
+  <React.StrictMode>    
+    <AuthProvider>
+      <BrowserRouter basename="/adventure-game">
+        <App />
+      </BrowserRouter>
+    </AuthProvider>    
   </React.StrictMode>
+
+  // <React.StrictMode>
+  //   <BrowserRouter basename="/adventure-game">
+  //     <App />
+  //   </BrowserRouter>
+  // </React.StrictMode>
 );
 
